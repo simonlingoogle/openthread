78a0bdeed3deedf21d9b203615b86304ba4fea54
Jiacheng Guo <gjc@google.com>
2020-02-27 14:26:44 +0800

    Enable otbr daemon mode
    * Increase number of state change handlers to 3.
    * Enable daemon mode for uno project.
    * Enable ot-ctl
    
    Depends-On: eureka-internal/348619
    
    Bug: 145246303
    Test: None
    Change-Id: Ice7bb40c90132fee8058960b0b01cb54a0ee9e63

--- third_party/nest/platforms/castos/thread.mk
+++ third_party/nest/platforms/castos/thread.mk
@@ -4,9 +4,12 @@ OPENTHREAD_PROJECT_CFLAGS = -DOPENTHREAD_PROJECT_CORE_CONFIG_FILE=\"openthread-c
 OPENTHREAD_PROJECT_INCLUDES = external/openthread/third_party/nest/platforms/castos
 OPENTHREAD_PROJECT_SRC_FILES = third_party/nest/extension/ot-legacy-pairing-ext.cpp
 
+USE_OTBR_DAEMON := 1
+
 PRODUCT_PACKAGES += \
   ncp-ctl \
   ncp_spi_loader \
+  ot-ctl \
   ot-cli \
   ot-ncp \
   ot-ncp-app.bin \
